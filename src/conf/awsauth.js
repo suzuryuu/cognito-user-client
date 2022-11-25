8b852228b789a1ff50d2148af100805227018e40
RyoyaFukushima <190146@jc-21.jp>
2022-11-25 10:49:09 +0900

    リポジトリ作り直し

--- src/conf/awsauth.js
+++ src/conf/awsauth.js
@@ -1,7 +1,7 @@
 const awsConfiguration = {
     region: 'ap-northeast-1',
-    UserPoolId: 'ap-northeast-1_EjiHI9Rgc',
-    ClientId: 'u3g3tln9ndrpp5f1kf2v11f3n'
+    UserPoolId: 'ap-northeast-xxxxxxxx',
+    ClientId: 'xxxxxxxxxxxxxxxxxxxx'
 };
   
 export default awsConfiguration;
